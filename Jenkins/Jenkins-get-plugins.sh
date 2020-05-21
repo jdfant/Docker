bc9916220daa5dd63136007459cd214fc2659226
jd <jd@jdfant.com>
2020-05-21 14:53:54 -0600

    Add Jenkins

---
+++ Jenkins/Jenkins-get-plugins.sh
@@ -0,0 +1,6 @@
+#!/usr/local/bin/bash
+#
+# Retrieve and list (write to file) all installed plugins on remote Jenkins server
+#
+JENKINS_HOST=USERNAME:PASSWORD@JENKINS_URL:PORT
+curl -sSL "http://$JENKINS_HOST/pluginManager/api/xml?depth=1&xpath=/*/*/shortName|/*/*/version&wrapper=plugins" | perl -pe 's/.*?<shortName>([\w-]+).*?<version>([^<]+)()(<\/\w+>)+/\1 \2\n/g'|sed 's/ /:/' > ./Files/jenkins_plugins
